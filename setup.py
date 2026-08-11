3b8c05d890050fa530b7c0144b266264272392da
MengHanLOVE1027 <2193438288@qq.com>
2026-08-11 09:57:28 +0800

    feat: 动态获取版本号并添加构建与发布工作流

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import re
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# 从源码读取版本号（单一来源，避免与 easycheckupdate_plugin.py 重复）
+with open("src/endstone_easycheckupdate/easycheckupdate_plugin.py", "r", encoding="utf-8") as f:
+    content = f.read()
+    match = re.search(r'^plugin_version\s*=\s*"(.+?)"', content, re.M)
+    version = match.group(1) if match else "0.0.0"
+
 setuptools.setup(
     name="endstone-easycheckupdate",
-    version="0.1.3",
+    version=version,
     author="MengHanLOVE",
     url='https://github.com/MengHanLOVE1027',
     author_email="2193438288@qq.com",
