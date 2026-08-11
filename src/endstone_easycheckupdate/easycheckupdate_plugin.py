e68f7d045a51ede150a0e9045414633024b9beaf
MengHanLOVE1027 <2193438288@qq.com>
2026-08-11 11:03:58 +0800

    feat: 更新版本至0.2.0-beta.3，修复命令usages子命令解析冲突并优化info/update子命令参数

--- src/endstone_easycheckupdate/easycheckupdate_plugin.py
+++ src/endstone_easycheckupdate/easycheckupdate_plugin.py
@@ -19,7 +19,7 @@ from .bstats import BStats, SimplePie
 plugin_name = "EasyCheckUpdate"
 plugin_name_smallest = "easycheckupdate"
 plugin_description = "一个基于 EndStone 的插件更新检查工具 / A plugin update checker based on EndStone."
-plugin_version = "0.2.0-beta.2"
+plugin_version = "0.2.0-beta.3"
 plugin_author = ["梦涵LOVE"]
 plugin_website = "https://www.minebbs.com/resources/easycheckupdate-ecu-endstone.15500/"
 plugin_github_link = "https://github.com/MengHanLOVE1027/endstone-easycheckupdate"
@@ -594,7 +594,6 @@ class EasyCheckUpdatePlugin(Plugin):
                 "/easycheckupdate",
                 "/easycheckupdate all",
                 "/easycheckupdate reload",
-                "/easycheckupdate <plugin_name: str>",
                 "/easycheckupdate update <plugin_name: str> [version: str]",
                 "/easycheckupdate info <plugin_name: str> [version: str]",
             ],
